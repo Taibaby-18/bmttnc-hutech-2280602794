959bfc987d9b24339a5bb6746cad7368a6903c97
Taibaby-18 <ngotantai18092004@gmail.com>
2025-05-28 23:00:58 +0700

    [add] lab02 railfence

--- lab-02/api.py
+++ lab-02/api.py
@@ -3,24 +3,28 @@ from cipher.vigenere import VigenereCipher  # Thêm vào phần đầu của fil
 
 app = Flask(__name__)  # ← DÒNG QUAN TRỌNG BỊ THIẾU
 
-# VIGENERE CIPHER ALGORITHM
-vigenere_cipher = VigenereCipher()
+from cipher.railfence import RailFenceCipher  # Thêm vào phần đầu của file api.py
 
-@app.route('/api/vigenere/encrypt', methods=['POST'])
-def vigenere_encrypt():
+# Thêm đoạn sau vào trước hàm main
+# RAILFENCE CIPHER ALGORITHM
+railfence_cipher = RailFenceCipher()
+
+@app.route('/api/railfence/encrypt', methods=['POST'])
+def encrypt():
     data = request.json
     plain_text = data['plain_text']
-    key = data['key']
-    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
     return jsonify({'encrypted_text': encrypted_text})
 
-@app.route('/api/vigenere/decrypt', methods=['POST'])
-def vigenere_decrypt():
+@app.route('/api/railfence/decrypt', methods=['POST'])
+def decrypt():
     data = request.json
     cipher_text = data['cipher_text']
-    key = data['key']
-    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
